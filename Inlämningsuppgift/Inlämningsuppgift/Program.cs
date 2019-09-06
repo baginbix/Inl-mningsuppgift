6c769fe91b7b17ff51e0313bbc332a739ea9dd19
timtuv001 <timtuv001@APC115104>
2019-09-06 08:49:04 +0200

    Lagt till projekt

---
+++ Inlämningsuppgift/Inlämningsuppgift/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Inlämningsuppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
